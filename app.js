6052effd3cced83997be01a769973445ea93c033
Cristian fabiano Eisenhut <cristiancfe@gmail.com>
2024-02-17 16:16:51 -0300

    formulario

---
+++ app.js
@@ -0,0 +1,6 @@
+// document.write('estou escrevendo na página ')
+// var sair = confirm(" Deseja sair da página e ser redirecionado ao Google?");
+// if (sair==1) {
+//     window.location = "https://www.google.com.br";
+// }
+
