54640bd2933d41d4f57e98f3b61747798036c579
Cristian fabiano Eisenhut <cristiancfe@gmail.com>
2024-02-18 12:47:05 -0300

    atualização com carrossel marquee

--- app.js
+++ app.js
@@ -4,3 +4,16 @@
 //     window.location = "https://www.google.com.br";
 // }
 
+//carrega mensagem no body
+// document.getElementById("inserir").innerHTML += "mensagem no Body do HTML";
+
+// carregando imagem local
+//    function inserir(){
+    document.getElementById("inserir").innerHTML = "<img src=' img/recruta.jpg' width='250'><br><br>";
+//    }
+   
+//carregando imagem externa 
+// function externa(){
+    document.getElementById("externa").innerHTML = "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4vTf22pUena56zrqrhPKzxBnXER51nCegBA&usqp=CAU' >";
+// }
+    
